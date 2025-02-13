5fa3539f57043acec2e9956f127c526e3fb05e61
Luka <lukaneek@gmail.com>
2025-02-13 15:39:03 -0600

    ci and cd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    return res.send("Luka Rocks server");
+    return res.send("Luka Rocks serveffr");
 });
 
 app.listen(8080, function(){
